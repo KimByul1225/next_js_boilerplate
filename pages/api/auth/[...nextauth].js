73063441178454c22d8288bd2e1da5539b04cc83
ByulKim <kimbyul1225@gmail.com>
2023-08-28 23:56:39 +0900

    세션 로그인 구현

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { connectDB } from "@/util/database";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 
 export const authOptions = {
     providers: [
@@ -8,6 +10,12 @@ export const authOptions = {
             clientSecret: process.env.GITHUB_CLIENT_SECRETS,
         }),
     ],
-    secret: process.env.JWT_SECRET
+    secret: process.env.JWT_SECRET,
+    adapter: MongoDBAdapter(connectDB), //세션 로그인용
 };
 export default NextAuth(authOptions); 
+
+
+
+
+
