b903c43f3debfa47934bb2701238cc064537afdd
ByulKim <byulkim1225@gmail.com>
2023-09-04 23:38:57 +0900

    create기능 로그인 정보 추가

--- pages/api/post/new.js
+++ pages/api/post/new.js
@@ -1,7 +1,16 @@
 import { connectDB } from "@/util/database";
-
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]";
 
 export default async function handler(req, res) {
+    let session = await getServerSession(req, res, authOptions);
+    // getServerSession을 사용해서 로그인한 유저의 정보를 불러옴
+    console.log(session);
+    if(session) {
+        req.body.author = session.user.email
+        // 요청 body에 author을 추가해서 로그인한 유저의 email정보를 추가
+    }
+
     if (req.method == 'POST') {
         if (req.body.title == '') {
             return res.status(500).json('제목 작성이 필요합니다.')
@@ -18,5 +27,3 @@ export default async function handler(req, res) {
         }
     }
 }
-
-
