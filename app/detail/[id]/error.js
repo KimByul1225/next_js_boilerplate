c6383ff1e159419acdcf1a8328f785b6d9c8c486
ByulKim <byulkim1225@gmail.com>
2023-09-17 18:31:14 +0900

    에러페이지 프롭스 확인

--- app/detail/[id]/error.js
+++ app/detail/[id]/error.js
@@ -2,7 +2,12 @@
 //에러페이지는 반드시 클라이언트 컴포넌트로 만들어야함.
 
 export default function Error({error, reset}) {
+    // error props는 에러 정보
+    // reset props는 함수로 페이지를 다시 로드
     return (
-        <div>에러페이지</div>
+        <div>
+            <h4>에러페이지</h4>
+            <button onClick={() => { reset() }}>페이지 다시 로드 하기</button>
+        </div>
     )
 }
