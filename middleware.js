901ac628cb5f1181aa618396d5becd568d2ab976
ByulKim <byulkim1225@gmail.com>
2024-05-03 22:20:13 +0900

    middleware 쿠키 다루기

--- middleware.js
+++ middleware.js
@@ -1,13 +1,16 @@
 import { NextResponse } from 'next/server';
-import { getToken } from "next-auth/jwt";
-
 export async function middleware(request) {
 
-    if (request.nextUrl.pathname.startsWith('/write')) {
-        const session = await getToken({ req: request })
-        console.log('세션', session)
-        if (session == null) {
-            return NextResponse.redirect(new URL('/api/auth/signin', request.url));
-        }
-    }
+    request.cookies.get('쿠키이름')  //출력
+    request.cookies.has('쿠키이름')  //존재확인
+    request.cookies.delete('쿠키이름')  //삭제
+
+    const response = NextResponse.next()
+    response.cookies.set({
+        name: 'mode',
+        value: 'dark',
+        maxAge: 3600,
+        httpOnly: true
+    })
+    return response  //쿠키생성
 } 
